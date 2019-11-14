8d66451ef5600635f23e7a699c73cb951a72a71b
liweiye666 <1320268524@qq.com>
2019-11-14 21:08:46 +0800

    终极版

--- src/main/java/com/example/service/StudentService.java
+++ src/main/java/com/example/service/StudentService.java
@@ -6,14 +6,14 @@ import com.example.domain.Student;
 import java.util.List;
 
 public interface StudentService {
-    public Student findStudent(String sid);
-    public List<Student> studentList(String id);
-    public void registerStudent(Student student);
-    public void registerMStudent(MStudent mStudent);
+    public Student findStudent(String sid);                 //找到学生对应的学委学号
+    public List<Student> studentList(String id);             //返回学生的集合
+    public void registerStudent(Student student);               //学生注册
+    public void registerMStudent(MStudent mStudent);            //学委注册
     public MStudent findMStudent(String id);
-    public String findCName(String id);
-    public List<Student> selectStudent(String sname);
-    public List<String> selectSName(String id);
-    public void updateSPWD(String sid,String pwd);
-    public void updatePWD(String id,String pwd);
+    public String findCName(String id);                      //根据学委学号找到对应的班级名
+    public List<Student> selectStudent(String sname);          //根据学生的姓名找到对应的学生信息
+    public List<String> selectSName(String id);              //根据学委学号找到所有的学生姓名
+    public void updateSPWD(String sid,String pwd);           //学生修改密码处理
+    public void updatePWD(String id,String pwd);             //学委修改密码处理
 }
